dd06876372fb9568e76c1e021ba9891304fd543c
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-27 20:27:30 +0700

    Membuat element input form

---
+++ src/elements/input/InputForm.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React, {forwardRef, ForwardedRef} from "react";
+import Label from "./Label";
+import Input from "./Input";
+
+// Menentukan tipe data untuk properti
+type InputFormProps = {
+  label?: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+  className?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void; // didefinisikan dengan tipe data untuk konsistensi
+  value?: string | number;
+  accept?: string;
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void; // didefinisikan dengan tipe data untuk konsistensi
+  error?: boolean;
+  helperText?: string;
+  checked?: boolean;
+};
+
+// memastikan bahwa ref diarahkan ke elemen input HTML yang sesuai.
+const InputForm = forwardRef<HTMLInputElement, InputFormProps>(
+  (
+    {
+      label,
+      name,
+      type = "text", // memiliki nilai default "text"
+      placeholder,
+      className,
+      onChange,
+      value,
+      accept,
+      onBlur,
+      error,
+      helperText,
+      checked,
+    },
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    return (
+      <div className="mb-3">
+        <Label htmlFor={name}>{label}</Label>
+        <Input
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          ref={ref}
+          className={className}
+          onChange={onChange}
+          value={value}
+          accept={accept}
+          onBlur={onBlur}
+          error={error}
+          helperText={helperText}
+          checked={checked}
+        />
+      </div>
+    );
+  }
+);
+
+export default InputForm;
