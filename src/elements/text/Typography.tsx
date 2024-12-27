479803ce313c8549ee8c73580596394f88ad3726
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-27 20:26:53 +0700

    Membuat element typography

---
+++ src/elements/text/Typography.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+type TextProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Typography: React.FC<TextProps> = ({children, className}) => {
+  return <p className={`font-poppins ${className}`}>{children}</p>;
+};
+
+export default Typography;
