2144e33d72e3b4ed02fbdf8dc38de5a56cc14398
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-27 20:27:05 +0700

    Membuat element label

---
+++ src/elements/input/Label.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+
+// definisikan tipe untuk properti props
+type LabelProps = {
+  children: React.ReactNode;
+  htmlFor?: string;
+  key?: React.Key; // Tambahkan tipe React.Key
+  className?: string;
+};
+
+const Label: React.FC<LabelProps> = ({
+  children,
+  htmlFor,
+  className,
+  // key, // Tidak perlu diambil karena 'key' tidak digunakan dalam JSX
+}) => {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={`block text-slate-700 text-sm font-bold mb-2 ${className}`}
+    >
+      {children}
+    </label>
+  );
+};
+
+export default Label;
