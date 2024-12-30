ad0b6cff45f9ad0b59b7ac993f9ab135eb4613fc
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-30 12:05:54 +0700

    Memanggil footer pada page utama

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Banner from "@/components/front-end/Banner";
 import Cart from "@/components/front-end/Cart";
 import Feature from "@/components/front-end/Feature";
+import Footer from "@/components/front-end/Footer";
 import Hero from "@/components/front-end/Hero";
 import Navbar from "@/components/front-end/Navbar";
 import TrendingProducts from "@/components/front-end/TrendingProducts";
@@ -19,6 +20,7 @@ export default function Home() {
       <Feature />
       <TrendingProducts />
       <Banner />
+      <Footer />
     </main>
   );
 }
