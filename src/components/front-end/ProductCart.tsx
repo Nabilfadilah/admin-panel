f047efe6de4ececace132c65b62eebc90c73702f
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 09:28:15 +0700

    Merubah tampilan card product

--- src/components/front-end/ProductCart.tsx
+++ src/components/front-end/ProductCart.tsx
@@ -30,14 +30,14 @@ const ProductCart = ({id, img, category, title, price}: propsType) => {
   };
 
   return (
-    <div className="border border-gray-200">
+    <div className="border border-gray-400 rounded-md">
       <div className="text-center border-b border-gray-200">
-        <img className="inline-block" src={img} alt={title} />
+        <img className="inline-block rounded-md" src={img} alt={title} />
       </div>
 
       <div className="px-8 py-4">
         <p className="text-gray-500 text-[14px] font-medium">{category}</p>
-        <h2 className="font-medium">{title}</h2>
+        <h2 className="font-medium text-lg">{title}</h2>
 
         <div className="mt-3 flex text-[#FFB21D] items-center">
           <AiFillStar />
@@ -49,9 +49,9 @@ const ProductCart = ({id, img, category, title, price}: propsType) => {
         </div>
 
         <div className="flex justify-between items-center mt-4">
-          <h2 className="font-medium text-accent text-xl">${price}</h2>
+          <h2 className="font-medium text-accent text-xl">Rp {price}</h2>
           <div
-            className="flex gap-2 items-center bg-pink text-white px-4 py-2 cursor-pointer hover:bg-accent"
+            className="flex gap-2 items-center rounded bg-cyan-900 text-white px-4 py-2 cursor-pointer hover:bg-cyan-800"
             onClick={addProductToCart}
           >
             <AiOutlineShoppingCart /> Add To Cart
