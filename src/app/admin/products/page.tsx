44c072e623873fd56949dbd418d3f2bba6c533eb
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 08:20:28 +0700

    Merubah ukuran width form product

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -3,7 +3,7 @@ import ProductForm from "@/components/admin-panel/ProductForm";
 const page = () => {
   return (
     <div className="h-[calc(100vh-96px)] w-full grid place-items-center overflow-y-auto">
-      <div className="bg-white w-[300px] rounded-lg p-4">
+      <div className="bg-white w-full rounded-lg p-4">
         <ProductForm />
       </div>
     </div>
