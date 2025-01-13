45317ae8076ff5841a98a0c7ad10dcf76c916d77
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 08:33:19 +0700

    Merubah icon cart dan background color button

--- src/components/front-end/Cart.tsx
+++ src/components/front-end/Cart.tsx
@@ -2,6 +2,9 @@ import {useAppSelector} from "@/redux/hooks";
 import React, {Dispatch, SetStateAction} from "react";
 import {RxCross1} from "react-icons/rx";
 import CartProduct from "./CartProduct";
+import {IoIosCloseCircleOutline} from "react-icons/io";
+import {FaShoppingCart} from "react-icons/fa";
+import {IoBagCheckOutline} from "react-icons/io5";
 
 interface PropsType {
   setShowCart: Dispatch<SetStateAction<boolean>>;
@@ -23,12 +26,12 @@ const Cart = ({setShowCart}: any) => {
   return (
     <div className="bg-slate-900/50 w-full min-h-screen fixed left-0 top-0 z-20 overflow-y-scroll">
       <div className="max-w-[400px] w-full min-h-full bg-white absolute right-0 top-0 p-6">
-        <RxCross1
-          className="absolute right-0 top-0 m-6 text-[24px] cursor-pointer"
+        <IoIosCloseCircleOutline
+          className="absolute text-3xl text-gray-600 top-4 right-4 cursor-pointer hover:text-red-600"
           onClick={() => setShowCart(false)}
         />
         <h3 className="pt-6 text-lg font-medium text-gray-600 uppercase">
-          Your Cart
+          Keranjang Anda
         </h3>
 
         <div className="mt-6 space-y-2">
@@ -46,15 +49,17 @@ const Cart = ({setShowCart}: any) => {
 
         <div className="flex justify-between items-center font-medium text-xl py-4">
           <p>Total:</p>
-          <p>${total}.00</p>
+          <p>Rp {total}.000</p>
         </div>
 
-        <button className="bg-black text-white text-center w-full rounded-3xl py-2 hover:bg-accent mb-4 mt-4">
-          View Cart
+        <button className="bg-cyan-900 text-white font-semibold text-center w-full rounded-3xl py-2 hover:bg-cyan-800 mb-4 mt-4 flex items-center justify-center gap-2">
+          <FaShoppingCart />
+          <span>View Cart</span>
         </button>
 
-        <button className="bg-black text-white text-center w-full rounded-3xl py-2 hover:bg-accent mb-4 mt-4">
-          CheckOut
+        <button className="bg-cyan-900 text-white font-semibold text-center w-full rounded-3xl py-2 hover:bg-cyan-800 mb-4 mt-4 flex items-center justify-center gap-2">
+          <IoBagCheckOutline />
+          <span>CheckOut</span>
         </button>
       </div>
     </div>
