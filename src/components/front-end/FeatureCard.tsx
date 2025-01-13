38dbb61348714445d615fede41fb9421f3c48d52
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 09:10:54 +0700

    Merubah background color dan text pada feature card

--- src/components/front-end/FeatureCard.tsx
+++ src/components/front-end/FeatureCard.tsx
@@ -8,12 +8,12 @@ interface propsType {
 
 const FeatureCard = ({icon, title, desc}: propsType) => {
   return (
-    <div className="flex gap-2 bg-gray-100 px-4 py-6">
+    <div className="flex gap-2 bg-cyan-800 rounded px-4 py-6 items-center justify-center">
       {icon}
 
       <div>
-        <h2 className="font-medium text-xl">{title}</h2>
-        <p className="text-gray-600">{desc}</p>
+        <h2 className="font-medium text-xl text-white">{title}</h2>
+        <p className="text-gray-200">{desc}</p>
       </div>
     </div>
   );
