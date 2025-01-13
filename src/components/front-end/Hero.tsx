05a42790bd4cd5a650864a2628a54868f070a5bc
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 09:10:19 +0700

    Merubah image hero dan tulisan hero

--- src/components/front-end/Hero.tsx
+++ src/components/front-end/Hero.tsx
@@ -2,33 +2,37 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="bg-[#E3EDF6] mt-4">
+    <div className="bg-[#E3EDF6]">
       <div className="container grid md:grid-cols-2 py-8">
         <div className="flex items-center">
           <div className="max-w-[450px] space-y-4">
             <p className="text-topHeadingSecondary">
-              Starting at <span className="font-bold">$999.00</span>
+              Dimulai dari <span className="font-bold">Rp 99.000</span>
             </p>
 
             <h1 className="text-topHeadingPrimary font-bold text-4xl md:text-5xl">
-              The best note bool collection 2024
+              Koleksi product terbaik 2025
             </h1>
 
-            <h3 className="text-2xl font-['Oregano', cursive]">
-              Exclusive offer <span>-10</span> off this week
+            <h3 className="text-xl font-['Oregano', cursive]">
+              Penawaran eksklusif <span>diskon 10%</span> minggu ini
             </h3>
 
             <a
-              className="inline-block bg-white rounded-md px-6 py-3 hover:bg-accent hover:text-white"
+              className="inline-block bg-cyan-900 text-white font-semibold rounded-md px-6 py-3 hover:bg-white hover:text-black"
               href="#"
             >
-              Shop Now
+              Belanja Sekarang
             </a>
           </div>
         </div>
 
         <div>
-          <img className="ml-auto" src="/avatar.png" alt="hero" />
+          <img
+            className="ml-auto w-[390px] h-[390px]"
+            src="/hero.png"
+            alt="hero"
+          />
         </div>
       </div>
     </div>
