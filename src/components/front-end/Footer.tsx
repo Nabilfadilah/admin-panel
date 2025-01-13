c2f3f8325ebde6168a9d2affe52fefaa8f8172c1
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 09:36:26 +0700

    Merubah baground color footer

--- src/components/front-end/Footer.tsx
+++ src/components/front-end/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-accent text-white p-4 text-center mt-16">
-      &copy; Copyright | All Rights Reserved <br /> The Brave Coders
+    <div className="bg-cyan-950 text-white font-semibold p-4 text-center mt-16">
+      &copy;Copyright | All Rights Reserved <br /> BilHut
     </div>
   );
 };
