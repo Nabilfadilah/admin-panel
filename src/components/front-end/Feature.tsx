6bc8c8e45ab4cde456664c1e617cb38c4492f8fd
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 09:11:19 +0700

    Merubah nama card fitur dan background color

--- src/components/front-end/Feature.tsx
+++ src/components/front-end/Feature.tsx
@@ -7,29 +7,29 @@ import FeatureCard from "./FeatureCard";
 const data = [
   {
     icon: <TbTruckDelivery className="text-4xl" />,
-    title: "Free Delivery",
-    desc: "Order from all item",
+    title: "Pengiriman Gratis",
+    desc: "Pesan dari semua item",
   },
   {
     icon: <RiRefund2Fill className="text-4xl" />,
-    title: "Return & Refund",
-    desc: "Money back guarantee",
+    title: "Pengembalian Dana",
+    desc: "Jaminan uang kembali",
   },
   {
     icon: <TbDiscount className="text-4xl" />,
-    title: "Member Discount",
-    desc: "On Order Over $99.00",
+    title: "Diskon Member",
+    desc: "Pesanan lebih dari Rp 99.000",
   },
   {
     icon: <MdSupportAgent className="text-4xl" />,
-    title: "Free Delivery",
-    desc: "Order from all item",
+    title: "Respon Cepat",
+    desc: "Hubungi kami",
   },
 ];
 
 const Feature = () => {
   return (
-    <div className="container grid gap-1 sm:grid-cols-2 lg:grid-cols-4 mt-8">
+    <div className="container grid gap-1 sm:grid-cols-2 lg:grid-cols-4 mt-8 text-white">
       {data.map((item) => (
         <FeatureCard
           key={item.title}
